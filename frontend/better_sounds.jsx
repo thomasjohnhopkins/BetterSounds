5169b0ca1f554fb756e955a2f8c9cce4b9f755f8
thomasjohnhopkins <thomas.john.hopkins@gmail.com>
2016-01-26 12:30:25 -0500

    Add folders to front end. Add dispatcher.

--- frontend/better_sounds.jsx
+++ frontend/better_sounds.jsx
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+
 var App = React.createClass({
   render: function () {
     return(
@@ -8,6 +13,12 @@ var App = React.createClass({
     );
   }
 });
+
+var routes = (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home}/>
+  </Route>
+);
 
 document.addEventListener("DOMContentLoaded", function () {
   ReactDOM.render(<App />, document.getElementById('content'));
