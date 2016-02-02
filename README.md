a8a02e1030c1a9e1e3cfceb727337000c8dd2ce0
thomasjohnhopkins <thomas.john.hopkins@gmail.com>
2016-02-01 21:26:28 -0500

    User websites now link to sites outside of the app.

--- README.md
+++ README.md
@@ -5,9 +5,6 @@ organize all of your favorite tracks in one place. You can upload your own music
 and discover artists with similar tastes.  This web application is inspired by
 SoundCloud and built using Ruby on Rails and React.js.
 
-
-
-
 ## Features
 
 BetterSounds allows users to:
@@ -16,3 +13,5 @@ BetterSounds allows users to:
 - Stream music through an audio player that persists as you navigate the site.
 - Upload your own audio files.
 - Comment on the audio files of others.
+
+## Project Design
--- frontend/components/user/userDetails.jsx
+++ frontend/components/user/userDetails.jsx
@@ -3,11 +3,14 @@ var UserStore = require('../../stores/currentUser');
 
 var UserDetails = React.createClass({
   render: function () {
-
+    var link = "http://" + this.props.user.website;
     return (
       <div className="user-details">
         <p className="user-details-item">
-          <a href="#">{this.props.user.website}</a>
+          <a className="website"
+            href={link}>
+              {this.props.user.website}
+          </a>
         </p>
         <p className="user-details-item">
           {this.props.user.bio}
