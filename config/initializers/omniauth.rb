0fc35b1961be161b4426da884d2a91de73ce8c15
thomasjohnhopkins <thomas.john.hopkins@gmail.com>
2016-02-20 17:50:18 -0500

    Yet another twitter oauth bug fix.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :twitter, ENV["API_KEY"], ["API_SECRET"]
+  provider :twitter, ENV['API_KEY'], ENV['API_SECRET']
 end
